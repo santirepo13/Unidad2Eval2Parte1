169e154206515ade1b61e3c7d7b5a498976a4df2
santirepo13 <tiagotorrespersonal@gmail.com>
2025-10-16 19:17:37 -0500

    finalizacion clase figura

--- WindowsFormsApp1/Figura.cs
+++ WindowsFormsApp1/Figura.cs
@@ -9,25 +9,44 @@ namespace WindowsFormsApp1
     public abstract class Figura
     {
 
-        //getters y setters para la posicion de la figura
+        // get/set para la posición de la figura
         public int X { get; set; }
         public int Y { get; set; }
 
-        // ancho y alto para las figuras
-        public int Width { get; set; }
-        public int Height { get; set; }
+        // ancho  y alto para las figuras
+        public int Ancho { get; set; }
+        public int Alto { get; set; }
 
-        // Propiedades de trazo/relleno
-        public System.Drawing.Color StrokeColor { get; set; } = System.Drawing.Color.Black;
-        public float StrokeWidth { get; set; } = 1f;
-        public bool Filled { get; set; } = false;
-        public System.Drawing.Color FillColor { get; set; } = System.Drawing.Color.LightGray;
+        // Propiedades de trazo/relleno 
+        public System.Drawing.Color ColorTrazo { get; set; } = System.Drawing.Color.Black;
+        public float GrosorTrazo { get; set; } = 1f;
+        public bool Rellenado { get; set; } = false;
+        public System.Drawing.Color ColorRelleno { get; set; } = System.Drawing.Color.LightGray;
 
-        // Identificador opcional
-        public string Name { get; set; }
+        // Identificador opcional (en español)
+        public string Nombre { get; set; }
 
-        // Método abstracto que las subclases deben implementar para dibujarse
-        public abstract void Dibujar(System.Drawing.Graphics g);
+        // Presets de color convenientes (en español)
+        public static System.Drawing.Color ColorAzul => System.Drawing.Color.Blue;
+        public static System.Drawing.Color ColorRojo => System.Drawing.Color.Red;
+
+        // Helpers para aplicar presets rápidamente 
+        public void UsarTrazoAzul() => ColorTrazo = ColorAzul;
+        public void UsarRellenoAzul()
+        {
+            ColorRelleno = ColorAzul;
+            Rellenado = true;
+        }
+
+        public void UsarTrazoRojo() => ColorTrazo = ColorRojo;
+        public void UsarRellenoRojo()
+        {
+            ColorRelleno = ColorRojo;
+            Rellenado = true;
+        }
+
+        
+                public abstract void Dibujar(System.Drawing.Graphics g);
 
     }
 }
